188b57ac19ea23c11bffe72f2a9eb511c44111f6
renaissanceee <st179481@stud.uni-stuttgart.de>
2024-10-21 09:35:57 +0200

    update

--- src/rendering.py
+++ src/rendering.py
@@ -3,19 +3,23 @@ import torch.nn.functional as F
 
 import math
 
-def cube_rendering(raw, pts, cnts, dx, dy, dz):
-    norm = torch.sqrt(torch.square(dx) + torch.square(dy) + torch.square(dz))
+def cube_rendering(raw, pts, cnts, dx, dy, dz): # raw: output of mlp
+    norm = torch.sqrt(torch.square(dx) + torch.square(dy) + torch.square(dz)) # 立方体的对角线长度
     raw2beta = lambda raw, dists, rs, act_fn=F.relu : -act_fn(raw) * dists * torch.square(rs) * 4 * math.pi
-    raw2alpha = lambda raw, dists, rs, act_fn=F.relu : (1.-torch.exp(-act_fn(raw) * dists)) * torch.square(rs) * 4 * math.pi 
-    rs = torch.norm(pts - cnts[:, None], dim=-1)
-    sorted_rs, indices_rs = torch.sort(rs)
-    dists = sorted_rs[...,1:] - sorted_rs[...,:-1]
-    dists = torch.cat([dists, dists[...,-1:]], -1)  # [N_rays, N_samples]
-    rgb = torch.gather(torch.sigmoid(raw[...,:-1]), -2, indices_rs[..., None].expand(raw[...,:-1].shape))
-    sorted_raw = torch.gather(raw[...,-1], -1, indices_rs)
-    beta = raw2beta(sorted_raw, dists, sorted_rs / norm)
-    alpha = raw2alpha(sorted_raw, dists, sorted_rs / norm)  # [N_rays, N_samples]
-    weights = alpha * torch.exp(torch.cumsum(torch.cat([torch.zeros(alpha.shape[0], 1), beta], -1), -1)[:, :-1])
+    raw2alpha = lambda raw, dists, rs, act_fn=F.relu : (1.-torch.exp(-act_fn(raw) * dists)) * torch.square(rs) * 4 * math.pi
+    # distance
+    rs = torch.norm(pts - cnts[:, None], dim=-1)# 采样点到中心的距离
+    sorted_rs, indices_rs = torch.sort(rs) # 排序
+    dists = sorted_rs[...,1:] - sorted_rs[...,:-1] # 相邻点的距离
+    dists = torch.cat([dists, dists[...,-1:]], -1)  # [N_rays, N_samples] 保持维度相同
+    # color
+    rgb = torch.gather(torch.sigmoid(raw[...,:-1]), -2, indices_rs[..., None].expand(raw[...,:-1].shape)) # c
+    # density
+    sorted_raw = torch.gather(raw[...,-1], -1, indices_rs) # σ
+    beta = raw2beta(sorted_raw, dists, sorted_rs / norm)# βi=−ReLU(rawi)⋅di⋅ri^2⋅4π  di:相邻点间距, ri:到center的归一化距离
+    alpha = raw2alpha(sorted_raw, dists, sorted_rs / norm)  # αi=(1−exp(−ReLU(rawi)⋅di))⋅ri^2⋅4π   #[N_rays, N_samples]
+    weights = alpha * torch.exp(torch.cumsum(torch.cat([torch.zeros(alpha.shape[0], 1), beta], -1), -1)[:, :-1]) # transmittance
+
     rgb_map = torch.sum(weights * rgb.squeeze(), -1)
 
     return {'rgb' : rgb_map, 'weights' : weights, 'indices_rs' : indices_rs}
--- src/sampling.py
+++ src/sampling.py
@@ -2,16 +2,18 @@ import torch
 import math
 
 def cube_sampling(batch, depths, n_samples, is_train, R):
-    n_cnts = batch.shape[0]
+    n_cnts = batch.shape[0] # 1024
     (cnts, LR, TB), (near, far) = torch.split(batch, [3, 2, 2], dim=-1), depths
 
     left, right = torch.split(LR, [1, 1], dim=-1)
     top, bottom = torch.split(TB, [1, 1], dim=-1)
-    steps = int(math.pow(n_samples, 1./3) + 1)
+    steps = int(math.pow(n_samples, 1./3) + 1)# 64->4^3, step=5
+    # (steps, steps, steps)
     t_vals = torch.cat([v[...,None] for v in torch.meshgrid(torch.linspace(0., 1., steps=steps), torch.linspace(0., 1., steps=steps), torch.linspace(0., 1., steps=steps))], -1)
-    t_vals = t_vals[1:, 1:, 1:].contiguous().view(-1, 3)
+    # (steps-1, steps-1, steps-1)
+    t_vals = t_vals[1:, 1:, 1:].contiguous().view(-1, 3) # exclude first sample
 
-    x_l, x_r = left.expand([n_cnts, n_samples]), right.expand([n_cnts, n_samples])
+    x_l, x_r = left.expand([n_cnts, n_samples]), right.expand([n_cnts, n_samples]) # [1024,1]->[1024,64] expand to build uniform samples
     y_l, y_r = top.expand([n_cnts, n_samples]), bottom.expand([n_cnts, n_samples])
     z_l = torch.full_like(x_l, 1.).view(-1, n_samples) * near[:, None]
     z_r = torch.full_like(x_r, 1.).view(-1, n_samples) * far[:, None]
